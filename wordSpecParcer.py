df8be541f789194c5ba0861042674872e1bd9640
Den <dengts@mail.ru>
2013-04-02 23:23:52 +0400

    add structure.txt for experemental. Some experemental with class section in wordSpecParcer.py

--- wordSpecParcer.py
+++ wordSpecParcer.py
@@ -7,14 +7,49 @@ class Section:
         self.members=[]
     def add(self,item):
         self.members.append(item)
+        return self.lastMember()
     def __getitem__(self, item):
         return self.members[item]
+    def __repr__(self):
+        return "section: '%s'(%i members)"%(self.__regExp,len(self))
+    def __len__(self):
+        return len(self.members)
+    def getOnlyItem(self):
+        items=[]
+        for item in self.members:
+            if not isinstance(item,Section):items.append(item)
+        return items
+    def getSubsections(self):
+        sections=[]
+        for section in self.members:
+            if isinstance(section,Section):sections.append(section)
+        return sections
+    def lastMember(self):
+        lastIndex=len(self.members)-1
+        if lastIndex>0:return self.members[lastIndex]
+        else:return 0
 
-reSection=re.compile(r"^сборочные единицы")
+def printSection(section,offset=0):
+    for member in section:
+        if isinstance(member,Section):
+            offset+=1
+            print "%s===%s==="%("\t"*offset,member)
+            printSection(member,offset)
+        else:print "%s%s"%("\t"*offset,member)
+if __name__=='__main__':
+    reSection=re.compile(r"^сборочные единицы")
+    testfile=open("testfile.txt","r")
+    line=testfile.readline()
+    noNameSection=Section(".")
 
-section=Section(r"сборочные единицы")
-section.add('test')
-section.add('сборочный чертеж')
-section.add(Section('Болт'))
-for i in section:
-    print i
+    section=Section(r"сборочные единицы")
+    section.add('test')
+    section.add(u'сборочный чертеж')
+    section.add(Section('Болт'))
+    subsection=section.getSubsections()[0]
+    subsection.add('sub')
+    subsection.add('Болт2')
+    subsection=subsection.add(Section('Винт'))
+    subsection.add('винт1')
+    printSection(section)
+    testfile.close()
